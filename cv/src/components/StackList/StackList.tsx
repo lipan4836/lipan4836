2bc5f4e4b1041beec929b544e71540a7bc26add5
Anton <lipan48@gmail.com>
2025-04-22 12:52:45 +0300

    feat: implement stack layout

--- cv/src/components/StackList/StackList.tsx
+++ cv/src/components/StackList/StackList.tsx
@@ -11,9 +11,31 @@ function StackList(): ReactElement {
         <button className={styles.btn}>State Management & Testing</button>
         <button className={styles.btn}>Tools</button>
       </div>
-      <div>
-        <div>
+      <div className={styles['stack-cont']}>
+        <div className={styles.slide}>
           <StackItem stackName={SpriteNames.STACK} idSprite="html" stackContent="HTML" />
+          <StackItem stackName={SpriteNames.STACK} idSprite="css" stackContent="CSS" />
+          <StackItem stackName={SpriteNames.STACK} idSprite="scss" stackContent="SCSS" />
+          <StackItem stackName={SpriteNames.STACK} idSprite="js" stackContent="JavaScript" />
+          <StackItem stackName={SpriteNames.STACK} idSprite="typescript" stackContent="TypeScript" />
+          <StackItem stackName={SpriteNames.STACK} idSprite="react" stackContent="React" />
+          <StackItem stackName={SpriteNames.STACK} idSprite="nextjs" stackContent="NEXT" />
+          <StackItem stackName={SpriteNames.STACK} idSprite="vue" stackContent="Vuejs" />
+        </div>
+        <div className={styles.slide}>
+          <StackItem stackName={SpriteNames.STACK} idSprite="redux" stackContent="Redux" />
+          <StackItem stackName={SpriteNames.STACK} idSprite="pinia" stackContent="Pinia" />
+          <StackItem stackName={SpriteNames.STACK} idSprite="jest" stackContent="Jest" />
+          <StackItem stackName={SpriteNames.STACK} idSprite="vitest" stackContent="Vitest" />
+        </div>
+        <div className={styles.slide}>
+          <StackItem stackName={SpriteNames.STACK} idSprite="git" stackContent="Git" />
+          <StackItem stackName={SpriteNames.SOCIAL} idSprite="github" stackContent="Github" />
+          <StackItem stackName={SpriteNames.STACK} idSprite="eslint" stackContent="EsLint" />
+          <StackItem stackName={SpriteNames.STACK} idSprite="prettier" stackContent="Prettier" />
+          <StackItem stackName={SpriteNames.STACK} idSprite="husky" stackContent="Husky" />
+          <StackItem stackName={SpriteNames.STACK} idSprite="netlify" stackContent="Netlify" />
+          <StackItem stackName={SpriteNames.STACK} idSprite="figma" stackContent="Figma" />
         </div>
       </div>
     </>
